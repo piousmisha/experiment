84c5985a29c767859e239ba3eee26c93c42f5d4f
piousmisha <56306957+piousmisha@users.noreply.github.com>
2019-10-12 17:03:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World, this is new Testpage generated from Jenkins - 12102019!')
+  response.send('My application is running successfully using Jenkins!')
 })
 
 app.listen(app.get('port'), function() {
